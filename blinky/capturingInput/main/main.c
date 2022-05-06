713287d8cfb38b574af5d948f9c36766d62c1299
prasannabe2004 <prasannabe2004@gmail.com>
2022-05-06 15:20:16 -0700

    Add reading input from keyboard project

---
+++ blinky/capturingInput/main/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include <string.h>
+
+void app_main(void)
+{
+    char c = 0;
+    char str[100];
+    memset(str, 0, sizeof(str));
+    while(c != '\n')
+    {
+        c = getchar();
+        if(c != 0xff)
+        {
+            str[strlen(str)] = c;
+            printf("%c", c);
+        }
+        vTaskDelay(100/portTICK_PERIOD_MS);
+    }
+    printf("you typed: %s", str);
+}
