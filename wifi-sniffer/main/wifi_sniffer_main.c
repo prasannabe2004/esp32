6de1801b38eecb1f4b5420141f0ba0599062807f
Prasanna Padmanaban Mohanasundaram <ppadmoh@amazon.com>
2025-02-17 16:58:36 +0530

    adding filters

--- wifi-sniffer/main/wifi_sniffer_main.c
+++ wifi-sniffer/main/wifi_sniffer_main.c
@@ -27,11 +27,99 @@ static void wifi_event_handler(void* arg, esp_event_base_t event_base,
         ESP_LOGI(TAG, "Got IP:" IPSTR, IP2STR(&event->ip_info.ip));
     }
 }
+#define SSID_MAX_LEN (32+1) //max length of a SSID
+#define MD5_LEN (32+1) //length of md5 hash
+#define BUFFSIZE 1024 //size of buffer used to send data to the server
+#define NROWS 11 //max rows that buffer can have inside send_data, it can be changed modifying BUFFSIZE
+#define MAX_FILES 3 //max number of files in SPIFFS partition
+typedef struct {
+	int16_t fctl; //frame control
+	int16_t duration; //duration id
+	uint8_t da[6]; //receiver address
+	uint8_t sa[6]; //sender address
+	uint8_t bssid[6]; //filtering address
+	int16_t seqctl; //sequence control
+	unsigned char payload[]; //network data
+} __attribute__((packed)) wifi_mgmt_hdr;
+static void get_ssid(unsigned char *data, char ssid[SSID_MAX_LEN], uint8_t ssid_len)
+{
+	int i, j;
 
+	for(i=26, j=0; i<26+ssid_len; i++, j++){
+		ssid[j] = data[i];
+	}
+
+	ssid[j] = '\0';
+}
+
+static int get_sn(unsigned char *data)
+{
+	int sn;
+    char num[5] = "\0";
+
+	sprintf(num, "%02x%02x", data[22], data[23]);
+    sscanf(num, "%x", &sn);
+
+    return sn;
+}
+
+static void get_ht_capabilites_info(unsigned char *data, char htci[5], int pkt_len, int ssid_len)
+{
+	int ht_start = 25+ssid_len+19;
+
+	/* 1) data[ht_start-1] is the byte that says if HT Capabilities is present or not (tag length).
+	 * 2) I need to check also that i'm not outside the payload: if HT Capabilities is not present in the packet,
+	 * for this reason i'm considering the ht_start must be lower than the total length of the packet less the last 4 bytes of FCS */
+
+	if(data[ht_start-1]>0 && ht_start<pkt_len-4){ //HT capabilities is present
+		if(data[ht_start-4] == 1) //DSSS parameter is set -> need to shift of three bytes
+			sprintf(htci, "%02x%02x", data[ht_start+3], data[ht_start+1+3]);
+		else
+			sprintf(htci, "%02x%02x", data[ht_start], data[ht_start+1]);
+	}
+}
 void wifi_sniffer_packet_handler(void* buff, wifi_promiscuous_pkt_type_t type)
 {
-    // This is where you process the sniffed packets
-    ESP_LOGI(TAG, "Packet sniffed!");
+	int pkt_len, fc, sn=0;
+	char ssid[SSID_MAX_LEN] = "\0", hash[MD5_LEN] = "\0", htci[5] = "\0";
+	uint8_t ssid_len;
+	time_t ts;
+
+	wifi_promiscuous_pkt_t *pkt = (wifi_promiscuous_pkt_t *)buff;
+	wifi_mgmt_hdr *mgmt = (wifi_mgmt_hdr *)pkt->payload;
+
+	fc = ntohs(mgmt->fctl);
+
+	if((fc & 0xFF00) == 0x4000){ //only look for probe request packets
+		time(&ts);
+
+		ssid_len = pkt->payload[25];
+		if(ssid_len > 0)
+			get_ssid(pkt->payload, ssid, ssid_len);
+
+		pkt_len = pkt->rx_ctrl.sig_len;
+		//get_hash(pkt->payload, pkt_len-4, hash);
+
+		sn = get_sn(pkt->payload);
+
+		get_ht_capabilites_info(pkt->payload, htci, pkt_len, ssid_len);
+		if(ssid_len > 1) {
+		ESP_LOGI(TAG, "ADDR=%02x:%02x:%02x:%02x:%02x:%02x, "
+				"SSID=%s, "
+				"TIMESTAMP=%d, "
+				"HASH=%s, "
+				"RSSI=%02d, "
+				"SN=%d, "
+				"HT CAP. INFO=%s",
+				mgmt->sa[0], mgmt->sa[1], mgmt->sa[2], mgmt->sa[3], mgmt->sa[4], mgmt->sa[5],
+				ssid,
+				(int)ts,
+				hash,
+				pkt->rx_ctrl.rssi,
+				sn,
+				htci);
+		}
+	}
 }
 
 void sniffer_task(void *pvParameters)
